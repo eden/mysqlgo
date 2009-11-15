588bb3728ca10dca9dbf296279245426b6f23b3e
Eden Li <eden@cpe-98-149-79-153.socal.res.rr.com>
2009-11-15 01:02:08 -0800

    Complete commits...

--- Makefile
+++ Makefile
@@ -2,12 +2,14 @@ include $(GOROOT)/src/Make.$(GOARCH)
 
 TARG=mysql
 CGOFILES=mysql.go
-CGO_CFLAGS=$(shell mysql_config --cflags)
-CGO_LDFLAGS=$(shell mysql_config --libs)
+MW_CFLAGS=$(shell mysql_config --cflags)
+CGO_LDFLAGS=mw.o $(shell mysql_config --libs)
 CLEANFILES+=main
 
 include $(GOROOT)/src/Make.pkg
 
-main: install main.go
-	$(GC) main.go
-	$(LD) -o $@ main.$O
+mysql_mysql.so: mw.o mysql.cgo4.o
+	gcc $(_CGO_CFLAGS_$(GOARCH)) $(_CGO_LDFLAGS_$(GOOS)) -o $@ mysql.cgo4.o $(CGO_LDFLAGS)
+
+mw.o: mw.c mw.h
+	gcc $(MW_CFLAGS) -o mw.o -c mw.c
--- mysql.go
+++ mysql.go
@@ -5,13 +5,8 @@
 // Implements rudimentary MySQL support.
 package mysql
 
-/*
-#include <stdlib.h>
-#include <mysql.h>
-
-char *itemAt(MYSQL_ROW row, unsigned int i) { return row[i]; }
-MYSQL_FIELD fieldAt(MYSQL_FIELD *field, unsigned int i) { return field[i]; }
-*/
+// #include <stdlib.h>
+// #include "mw.h"
 import "C"
 
 import "os"
@@ -19,16 +14,16 @@ import "fmt"
 import "unsafe"
 
 func init() {
-	C.mysql_library_init(0, nil, nil);
+	C.mw_library_init();
 }
 
 // ConnInfo represents MySQL connection information.
-//	- host: Host to connect to, passed directly into mysql_real_connect
+//	- host: Host to connect to, passed directly into mw_real_connect
 //	  which will resolve DNS names.
 //	- port: Port on which to connect.
-//	- uname: Username to use for the connection.
-//	- pass: Password to use for the connection.
-//	- dbname: Initial database to use after successfully connecting.
+//	- uname: Username to usemw for the connection.
+//	- pass: Password to usemw for the connection.
+//	- dbname: Initial database to usemw after successfully connecting.
 type ConnInfo struct {
 	host	string;
 	port	int;
@@ -39,47 +34,55 @@ type ConnInfo struct {
 
 // Conn maintains the connection state of a single MySQL connection.
 type Conn struct {
-	h	C.MYSQL;
-	connected	bool;
+	h	C.mw;
 }
 
 func NewConn() *Conn {
 	my := new(Conn);
-	C.mysql_init(&my.h);
-	my.connected = false;
+	my.h = nil;
 	return my;
 }
 
+func usemw(h C.mw) unsafe.Pointer { return unsafe.Pointer(h); }
+func usemwres(h C.mwres) unsafe.Pointer { return unsafe.Pointer(h); }
+func usemwrow(h C.mwrow) unsafe.Pointer { return unsafe.Pointer(h); }
+func usemwfield(h C.mwfield) unsafe.Pointer { return unsafe.Pointer(h); }
+
 // Returns the last error that occurred as an os.Error 
 func (my *Conn) LastError() os.Error {
-	if err := C.mysql_error(&my.h); *err != 0 {
+	if err := C.mw_error(usemw(my.h)); *err != 0 {
 		return os.NewError(C.GoString(err));
 	}
 	return nil;
 }
 
 // Connects to the server specified in the given connection info.
-func (my *Conn) Connect(conn *ConnInfo) err os.Error {
+func (my *Conn) Connect(conn *ConnInfo) (err os.Error) {
 	args := []*C.char{
 		C.CString(conn.host), C.CString(conn.uname),
 		C.CString(conn.pass), C.CString(conn.dbname)};
 
-	C.mysql_real_connect(
-		&my.h,
+	if (my.h != nil) {
+		my.Close();
+	}
+
+	my.h = C.mw_init(nil);
+	if my.h == nil { return os.ENOMEM }
+
+	C.mw_real_connect(
+		usemw(my.h),
 		args[0],
 		args[1],
 		args[2],
 		args[3],
-		C.uint(conn.port),
-		nil,
-		0);
+		C.int(conn.port));
 
 	for i, _ := range args {
 		C.free(unsafe.Pointer(args[i]))
 	}
 
 	err = my.LastError();
-	my.connected = err != nil;
+	if err != nil { my.h = nil };
 
 	return;
 }
@@ -87,7 +90,7 @@ func (my *Conn) Connect(conn *ConnInfo) err os.Error {
 // Returns a new cursor for the current connection.  If the connection
 // hasn't yet been established, nil is returned.
 func (my *Conn) Cursor() *Cursor {
-	if my.connected {
+	if my.h != nil {
 		return &Cursor{my, nil, 0};
 	}
 	return nil;
@@ -95,21 +98,20 @@ func (my *Conn) Cursor() *Cursor {
 
 // Closes and cleans up the connection.
 func (my *Conn) Close() {
-	C.mysql_close(&my.h);
-	my.connected = false;
+	C.mw_close(usemw(my.h));
 }
 
 // Wraps a MYSQL_RES structure and allows one to execute queries and
 // read results.
 type Cursor struct {
 	my	*Conn;
-	res	*C.MYSQL_RES;
+	res	C.mwres;
 	nfields	int;
 }
 
 func (c *Cursor) cleanup() {
 	if c.res != nil {
-		C.mysql_free_result(c.res);
+		C.mw_free_result(usemwres(c.res));
 		c.res = nil;
 		c.nfields = 0;
 	}
@@ -123,39 +125,38 @@ func (c *Cursor) Execute(query string, parameters ...) os.Error {
 	query = fmt.Sprintf(query, parameters);
 
 	// TODO figure out how to convert a string to a *C.char
-	// and use mysql_real_query instead (saves malloc/copy)
+	// and usemw mw_real_query instead (saves malloc/copy)
 	q := C.CString(query);
-	C.mysql_query(&c.my.h, q);
+	C.mw_query(usemw(c.my.h), q);
 	C.free(unsafe.Pointer(q));
 
-	c.res = C.mysql_store_result(&c.my.h);
-	c.nfields = int(C.mysql_num_fields(c.res));
+	c.res = C.mw_store_result(usemw(c.my.h));
+	c.nfields = int(C.mw_num_fields(usemwres(c.res)));
 
 	return c.my.LastError();
 }
 
 func (c *Cursor) Description() []Column {
 	columns := make([]Column, c.nfields);
-	fields := C.mysql_fetch_fields(c.res);
+	fields := C.mw_fetch_fields(usemwres(c.res));
 
 	for i := 0; i < c.nfields; i += 1 {
-		field := C.fieldAt(fields, C.uint(i));
 		columns[i] = Column{
-			C.GoString(field.name),
-			int(field._type)}
+			C.GoString(C.mw_field_name_at(usemwfield(fields), C.int(i))),
+			int(C.mw_field_type_at(usemwfield(fields), C.int(i)))}
 	}
 
 	return columns;
 }
 
 func (c *Cursor) FetchOne() (data []interface {}, err os.Error) {
-	row := C.mysql_fetch_row(c.res);
+	row := C.mw_fetch_row(usemwres(c.res));
 	err = c.my.LastError();
 
 	if row != nil && err == nil {
 		data = make([]interface {}, c.nfields);
 		for i := 0; i < c.nfields; i += 1 {
-			data[i] = C.GoString(C.itemAt(row, C.uint(i)));
+			data[i] = C.GoString(C.mw_row(usemwrow(row), C.int(i)));
 		}
 	}
 
@@ -183,7 +184,7 @@ func (c *Cursor) FetchAll() ([]interface {}, os.Error) {
 }
 
 func (c *Cursor) RowCount() int64 {
-	return int64(C.mysql_num_rows(c.res));
+	return int64(C.mw_num_rows(usemwres(c.res)));
 }
 
 func (c *Cursor) Close() {
