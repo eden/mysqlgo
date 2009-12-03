c2a0228c711c50314deea6876bb63b8f5926b4dd
Eden Li <eden.li@gmail.com>
2009-12-03 00:48:08 -0800

    add notice

--- example.go
+++ example.go
@@ -1,3 +1,9 @@
+// Copyright 2009 Eden Li. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Example usage of mysqlgo.
+
 package main
 
 import (
